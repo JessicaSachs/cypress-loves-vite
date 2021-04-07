909e9658a3b2ed04e6cb4d9773e6ef5ba5d6449f
Jessica Sachs <jess@jessicasachs.io>
2021-04-07 05:09:51 -0400

    removing pre-release patch-package workaround

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,22 +2,15 @@ import 'virtual:windi.css'
 import 'virtual:windi-devtools'
 import '../src/index.css'
 
-const $ = Cypress.$
-before(() => {
-  $($(window.parent.document.body).find('.Pane.horizontal:nth-child(3) div div')[0]).height(0)
-})
-
 beforeEach(() => {
   cy.viewport(600, 600, { log: false })
 })
-
 
 Cypress.Commands.overwrite('submit', function (original, $form) {
   const spy = cy.spy(() => {
   })
   if ($form) {
     $form[0].submit = spy
-
   }
   
   return cy.wrap(original(...Array.from(arguments).slice(1)), { log: false }).wrap(spy, { log: false })
